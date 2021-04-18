2df5a7946770e979e72bff6f13faec63cd6b73a0
unknown <cortinabryan@gmail.com>
2021-04-17 17:44:13 -0700

    created navbar menu toggle trigger, styled navbar dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const hambuger = document.getElementById("hamburger");
-const hamburgerLinks = document.getElementById("nav-ul");
+const hamburgerLinks = document.getElementById("hamburger-links");
 
 hamburger.addEventListener("click", () => {
   hamburgerLinks.classList.toggle("show");
 });
+
+// document
+//   .getElementById("hamburger-links")
+//   .addEventListener("click", openHamburger);
+
+// function openHamburger() {
+//   document.getElementsById("hamburger-links").classList.toggle("show");
+// }
+
+// hamburger.addEventListener("click", () => {
+//   hamburgerLinks.classList.toggle("show");
+// });
 
 // function toggle() {
 //   var x = document.getElementById("hamburger-links");
