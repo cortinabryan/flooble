c7f0c04796d72337becf36a4338cb955ccbc04df
Bryan <cortinabryan@gmail.com>
2021-06-13 12:14:08 -0700

    testing ssh

--- js/script.js
+++ js/script.js
@@ -4,26 +4,3 @@ const hamburgerLinks = document.getElementById("hamburger-links");
 hamburger.addEventListener("click", () => {
   hamburgerLinks.classList.toggle("show");
 });
-
-// document
-//   .getElementById("hamburger-links")
-//   .addEventListener("click", openHamburger);
-
-// function openHamburger() {
-//   document.getElementsById("hamburger-links").classList.toggle("show");
-// }
-
-// hamburger.addEventListener("click", () => {
-//   hamburgerLinks.classList.toggle("show");
-// });
-
-// function toggle() {
-//   var x = document.getElementById("hamburger-links");
-//   if (x.style.display === "flex") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "flex";
-//   }
-// }
-
-// toggle();
